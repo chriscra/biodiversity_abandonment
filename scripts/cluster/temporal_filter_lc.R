bb7597111f580dde867648861122576567612ec6
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2021-12-12 22:35:23 -0500

    Modify temporal filtering script to use dt_to_raster() function rather than terra equivalent

--- scripts/cluster/temporal_filter_lc.R
+++ scripts/cluster/temporal_filter_lc.R
@@ -1,11 +1,11 @@
 # ------------------------------------------------------------ #
 # Run temporal filter for raw land cover maps (Yin et al. 2020)
 # 
-# Christopher Crawford, Princeton University, December 6th, 2021
+# Christopher Crawford, Princeton University, December 12th, 2021
 # ------------------------------------------------------------ #
 # load libraries
 cluster_packages <- c("data.table", "tictoc", "raster", "terra",
-                      "sf", "fasterize",
+                      "sf", "fasterize", "dtraster",
                       "tidyverse", "rgdal")
 
 install_pkg <- lapply(cluster_packages, library, character.only = TRUE)
@@ -21,13 +21,18 @@ p_tmp           <- "/scratch/gpfs/clc6/biodiversity_abn/derived/tmp/"
 # source functions:
 source("/home/clc6/biodiversity_abn/scripts/util/_util_functions.R")
 
+# set terra autosave options:
+terraOptions(tempdir = p_tmp)
+rasterOptions(tmpdir = p_tmp)
+
+
 # # array set up -------
 args <- commandArgs(TRUE) # access the slurm array variable
 site_index <- as.numeric(args[1])
+# site_index <- 11
 
 # ------- load files -------- #
 site_df <- read.csv(file = paste0(p_dat, "site_df.csv"))
-
 
 # load the raw land cover data.table
 lc_dt <- fread(
@@ -35,8 +40,14 @@ lc_dt <- fread(
     p_input_rasters,
     site_df$site[site_index], ".csv"))
 
+print(paste0("ncell, before filter: ", nrow(lc_dt)))
+
 # pass temporal filter
+tic("pass temporal filter")
 cc_temporal_filter_lc(lc_dt)
+toc()
+
+print(paste0("ncell, after filter: ", nrow(lc_dt)))
 
 # write to file
 fwrite(lc_dt, 
@@ -44,7 +55,22 @@ fwrite(lc_dt,
          p_input_rasters,
          site_df$site[site_index], "_clean.csv"))
 
-cat(paste0("Success! Passed temporal filter on land cover data.table for site: ", site_df$site[site_index]))
+cat(paste0("Success! Passed temporal filter on land cover data.table for site: ", site_df$site[site_index]), fill = TRUE)
 
+# now, convert the cleaned data.table back to a spatraster:
+tic("translate cleaned lc_dt to raster")
+# r <- dt_to_spatraster(dt = lc_dt)
+r <- dt_to_raster(lc_dt, raster::crs("+proj=longlat +datum=WGS84 +no_defs"))
+toc()
 
+print(paste0("ncell of raster: ", ncell(r)))
 
+# write new cleaned lc raster to file
+tic("write new cleaned lc raster to file")
+raster::writeRaster(r,
+            filename = paste0(p_input_rasters,
+                              site_df$site[site_index], "_clean.tif"),
+            overwrite = TRUE)
+toc()
+
+cat(paste0("Success! Converted cleaned dt back to raster, and saved as file, for site: ", site_df$site[site_index]), fill = TRUE)
